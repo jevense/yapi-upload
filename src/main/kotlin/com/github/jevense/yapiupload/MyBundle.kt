2aceffa5952d8e093a292cb6f9636426051888f7
GitHub Action <action@github.com>
2023-01-03 07:21:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/jevense/yapiupload/MyBundle.kt
+++ src/main/kotlin/com/github/jevense/yapiupload/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jevense.yapiupload
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jevense/yapiupload/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jevense/yapiupload/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jevense.yapiupload.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jevense.yapiupload.MyBundle
 
 class MyApplicationService {
 
